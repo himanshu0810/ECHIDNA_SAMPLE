b56f337780e398399efa14ea8e026c5c28ae1cb8
Himanshu <sangalhimanshu08@gmail.com>
2024-06-29 12:45:38 +0530

    setting up the sample

---
+++ test/token.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Token} from "../src/token.sol";
+
+//test the functionality:
+//no user can have balance greater than 1000
+contract TestToken is Token {
+
+    constructor() public {
+
+    }
+
+    function echidna_balance_under_1000() public view returns(bool) {
+        return balances[msg.sender] <= 1000;
+    }
+}
